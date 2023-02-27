07fef55cfb9b10228659c3d4c339a2b1d304047d
FullStackSD <shazalohayo@hotmail.co.uk>
2023-02-27 09:18:09 +0000

    Modified items.html form with clear filters

--- item/views.py
+++ item/views.py
@@ -1,8 +1,29 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import NewItemForm, EditItemForm
-from .models import Item
+from .models import Category, Item
+
+
+def items(request):
+    query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False)
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q (description__icontains=query))
+
+    return render(request, 'item/items.html', {
+        'items': items,
+        'query': query,
+        'categories': categories,
+        'category_id': int(category_id)
+    })
 
 
 def detail(request, pk):
@@ -13,6 +34,7 @@ def detail(request, pk):
         'item': item,
         'related_items': related_items
     })
+
 
 @login_required
 def new(request):
@@ -32,6 +54,7 @@ def new(request):
         'form': form
     })
 
+
 @login_required
 def edit(request, pk):
     item = get_object_or_404(Item, pk=pk, created_by=request.user)
@@ -50,6 +73,7 @@ def edit(request, pk):
         'form': form,
         'title': 'Edit item',
     })
+
 
 @login_required
 def delete(request, pk):
